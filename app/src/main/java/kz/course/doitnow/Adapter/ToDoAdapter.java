bf8a62754c8966ea695653e265e18549449055b5
Balapashka <max.boostvink@gmail.com>
2021-03-06 12:52:45 +0600

    final release

--- app/src/main/java/kz/course/doitnow/Adapter/ToDoAdapter.java
+++ app/src/main/java/kz/course/doitnow/Adapter/ToDoAdapter.java
@@ -1,60 +1,103 @@
 package kz.course.doitnow.Adapter;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import kz.course.doitnow.AddNewTask;
 import kz.course.doitnow.MainActivity;
 import kz.course.doitnow.Model.ToDoModel;
 import kz.course.doitnow.R;
+import kz.course.doitnow.Utils.DatabaseHandler;
 
 public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     private List<ToDoModel> todoList;
+    private DatabaseHandler db;
     private MainActivity activity;
 
-    public ToDoAdapter(MainActivity activity){
+    public ToDoAdapter(DatabaseHandler db, MainActivity activity) {
+        this.db = db;
         this.activity = activity;
     }
 
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int ViewType){
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.task_layout, parent, false);
         return new ViewHolder(itemView);
     }
 
-    public void onBindViewHolder(ViewHolder holder, int position){
-        ToDoModel item = todoList.get(position);
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        db.openDatabase();
+
+        final ToDoModel item = todoList.get(position);
         holder.task.setText(item.getTask());
         holder.task.setChecked(toBoolean(item.getStatus()));
-
+        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    db.updateStatus(item.getId(), 1);
+                } else {
+                    db.updateStatus(item.getId(), 0);
+                }
+            }
+        });
     }
 
-    public int getItemCount(){
+    private boolean toBoolean(int n) {
+        return n != 0;
+    }
+
+    @Override
+    public int getItemCount() {
         return todoList.size();
     }
 
-    private boolean toBoolean(int n){
-        return n!=0;
+    public Context getContext() {
+        return activity;
     }
 
-    public void setTasks(List<ToDoModel> todoList){
+    public void setTasks(List<ToDoModel> todoList) {
         this.todoList = todoList;
         notifyDataSetChanged();
-    };
+    }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public void deleteItem(int position) {
+        ToDoModel item = todoList.get(position);
+        db.deleteTask(item.getId());
+        todoList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editItem(int position) {
+        ToDoModel item = todoList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", item.getId());
+        bundle.putString("task", item.getTask());
+        AddNewTask fragment = new AddNewTask();
+        fragment.setArguments(bundle);
+        fragment.show(activity.getSupportFragmentManager(), AddNewTask.TAG);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         CheckBox task;
 
-        ViewHolder(View view){
+        ViewHolder(View view) {
             super(view);
             task = view.findViewById(R.id.todoCheckBox);
         }
     }
-
 }
---
+++ app/src/main/java/kz/course/doitnow/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package kz.course.doitnow;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
